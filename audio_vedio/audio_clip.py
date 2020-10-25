4b2d21f420cedfb0eb0b824564caaba30063aa9f
Winter-is-coming-wow <1875786416@qq.com>
2020-10-25 21:35:04 +0800

    "change audio_clip.py"

--- audio_vedio/audio_clip.py
+++ audio_vedio/audio_clip.py
@@ -10,24 +10,55 @@ import dlib
 # audio.write_audiofile('cache/test.wav')
 
 #detector = dlib.get_frontal_face_detector()
-detector = dlib.cnn_face_detection_model_v1('cache/mmod_human_face_detector.dat/mmod_human_face_detector.dat')
-predector_landmarks=dlib.shape_predictor('cache/face_landmarks.dat')
+def detectface():
+    detector = dlib.cnn_face_detection_model_v1('cache/mmod_human_face_detector.dat/mmod_human_face_detector.dat')
+    #predector_landmarks=dlib.shape_predictor('cache/face_landmarks.dat')
 
-capture=cv.VideoCapture('cache/test.avi')
-while True:
-    ret,frame=capture.read()
-    if ret:
-        gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
-        faces=detector(gray,1)
-        for face in faces:
-            left = face.rect.left()
-            top = face.rect.top()
-            right = face.rect.right()
-            bottom = face.rect.bottom()
-            cv.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 3)
+    capture=cv.VideoCapture('cache/test.avi')
+    fps=capture.get(cv.CAP_PROP_FPS)
+    waittime=int(60.0/fps)
+    while True:
+        ret,frame=capture.read()
+        if ret:
+            gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+            faces=detector(gray,1)
+            for face in faces:
+                left = face.rect.left()
+                top = face.rect.top()
+                right = face.rect.right()
+                bottom = face.rect.bottom()
+                cv.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 3)
 
-        cv.imshow('audio',frame)
-        cv.waitKey(10)
+            cv.imshow('audio',frame)
+            key = cv.waitKey(waittime)
+            if key == 27:
+                break
 
-    else:
-        break
+        else:
+            break
+
+
+def detectface2():
+    path = 'cache/data/haarcascades/haarcascade_frontalface_default.xml'
+    detector = cv.CascadeClassifier(path)
+
+    capture = cv.VideoCapture('cache/test.avi')
+    fps=capture.get(cv.CAP_PROP_FPS)
+    waittime=int(60.0/fps)
+    while True:
+        ret, frame = capture.read()
+        if ret:
+            gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+            faces = detector.detectMultiScale(gray)
+            for x, y, w, h in faces:
+                cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            cv.imshow('audio', frame)
+            key=cv.waitKey(waittime)
+            if key==27:
+                break
+
+        else:
+            break
+
+if __name__ == '__main__':
+    detectface2()
